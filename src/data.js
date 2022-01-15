5d9912b050908538bd50f3022d5910cac39b677f
nguyengiathang190402 <94787151+nguyengiathang190402@users.noreply.github.com>
2022-01-15 14:58:46 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQde6jJlVw6nZRvCIUKV6ilH7n0sOEaemSzOA&usqp=CAU", desc: "Buổi lễ tôn vinh “Ong vàng Poly” học kỳ Spring 2018 của Cao đẳng FPT Polytechnic Hà Nội lần này có sự tham gia của thầy Vũ Chí Thành – Giám đốc khối Đào tạo Cao đẳng – FPT Polytechnic, thầy Lã Ngọc Quang – Trưởng ban Đào tạo khối Đào tạo Cao đẳng, anh Lê Quốc Nam – Trưởng phòng Marketting –  FPT Polytechnic",
+    },
+    {
+        id: "2", title: "Sinh viên khối ngành Kinh tế học cách 'va chạm' vào cảm xúc", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQde6jJlVw6nZRvCIUKV6ilH7n0sOEaemSzOA&usqp=CAU", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi những chia sẻ đáng quý từ chính kinh nghiệm bản thân của diễn giả: Trần Hồng Hải – Chủ tịch HĐQT Công ty CP Đào tạo và phát triển Công nghệ truyền thông HAITRAN.",
+    },
+    {
+        id: "3", title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQde6jJlVw6nZRvCIUKV6ilH7n0sOEaemSzOA&usqp=CAU", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp và tham gia các chương trình talkshow, chia sẻ kinh nghiệm cùng chuyên gia.",
+    },
+];
+export default data;
